e80e19c668e83812d909d952ed51f3f2c4230d17
boxton90 <boxton.da@gmail.com>
2018-11-16 17:43:11 +0400

    Integrating react-tabs

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import Product from './components/product'
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs'
 
+import 'react-tabs/style/react-tabs.css';
 import './styles.css'
 
 export default class ProductWidget extends Component {
@@ -24,13 +26,23 @@ export default class ProductWidget extends Component {
         <div className="modal-background" onClick={this.toggleModal}></div>
         <div className="modal-content">
           <div className="box">
-            <div className="tabs is-centered">
-              <ul>
-                <li className="is-active"><a>Product</a></li>
-                <li><a>Recomendations</a></li>
-              </ul>
-            </div>
-            <Product title="Product title" desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean efficitur sit amet massa fringilla egestas. Nullam condimentum luctus turpis."></Product>
+            <Tabs>
+              <TabList className="tabs is-fullwidth is-centered">
+                <Tab selectedClassName="is-active"><a>Product</a></Tab>
+                <Tab selectedClassName="is-active"><a>Recommendations</a></Tab>
+              </TabList>
+              <TabPanel>
+                <Product title="Product title" desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean efficitur sit amet massa fringilla egestas. Nullam condimentum luctus turpis."></Product>
+              </TabPanel>
+              <TabPanel>
+                <Product title="Product title" desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean efficitur sit amet massa fringilla egestas. Nullam condimentum luctus turpis."></Product>
+                <Product title="Product title" desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean efficitur sit amet massa fringilla egestas. Nullam condimentum luctus turpis."></Product>
+                <Product title="Product title" desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean efficitur sit amet massa fringilla egestas. Nullam condimentum luctus turpis."></Product>
+                <Product title="Product title" desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean efficitur sit amet massa fringilla egestas. Nullam condimentum luctus turpis."></Product>
+                <Product title="Product title" desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean efficitur sit amet massa fringilla egestas. Nullam condimentum luctus turpis."></Product>
+                <Product title="Product title" desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean efficitur sit amet massa fringilla egestas. Nullam condimentum luctus turpis."></Product>
+              </TabPanel>
+            </Tabs>
           </div>
         </div>
         <button className="modal-close is-large" aria-label="close" onClick={this.toggleModal}></button>
