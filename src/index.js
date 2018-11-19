d4b52da39dfad3d1aea84364f98a17137fdc1b2b
boxton90 <boxton.da@gmail.com>
2018-11-19 11:18:49 +0400

    Implementing onKeyPress event to handle switching between tabs with the arrow keys

--- src/index.js
+++ src/index.js
@@ -8,10 +8,45 @@ import 'react-tabs/style/react-tabs.css';
 import './styles.css'
 
 export default class ProductWidget extends Component {
-  static propTypes = {
-    text: PropTypes.string
+  constructor() {
+    super()
+    this.state = {
+      selectedTabIndex: 0,
+      numberTabs : 2}
+    this.onKeyDown = this.onKeyDown.bind(this)
   }
 
+  componentDidMount() {
+    document.addEventListener('keydown',this.onKeyDown)
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keydown')
+  }
+
+  /* OnkeyDown event that handle right and left arrow keys pressing to switch between tabs */
+  onKeyDown(event) {
+    switch (event.keyCode) {
+      case 39:
+        this.state.selectedTabIndex < (this.state.numberTabs - 1) ? this.increaseSelectedTabIndex() : null
+      break
+      case 37:
+        this.state.selectedTabIndex > 0 ? this.dicreaseSelectedTabIndex() : null
+      break
+    }
+  }
+
+  /* Method that increase selected tab index state count by 1 */
+  increaseSelectedTabIndex = () => {
+    this.setState(prevState => ({selectedTabIndex: prevState.selectedTabIndex + 1}))
+  }
+
+  /* Method that dicrease selected tab index state count by 1 */
+  dicreaseSelectedTabIndex = () => {
+    this.setState(prevState => ({selectedTabIndex: prevState.selectedTabIndex - 1}))
+  }
+
+  /* Method that toggle (open/close) widget modal */
   toggleModal = () => {
     const modal = document.querySelector('.modal')
     modal.classList.toggle('is-active')
@@ -23,7 +58,7 @@ export default class ProductWidget extends Component {
         <div className="modal-background" onClick={this.toggleModal}></div>
         <div className="modal-content">
           <div className="box">
-            <Tabs>
+            <Tabs selectedIndex={this.state.selectedTabIndex} onSelect={selectedTabIndex => this.setState({ selectedTabIndex: selectedTabIndex })}>
               <TabList className="tabs is-fullwidth is-centered">
                 <Tab selectedClassName="is-active"><a>Product</a></Tab>
                 <Tab selectedClassName="is-active"><a>Recommendations</a></Tab>
@@ -32,7 +67,7 @@ export default class ProductWidget extends Component {
                 <ProductItem title={this.props.productData.title} desc={this.props.productData.desc} img={this.props.productData.img}></ProductItem>
               </TabPanel>
               <TabPanel>
-                {this.props.recommendationsData.recommendations.map((recommendation,index) => {
+                {this.props.recommendationsData.recommendations.map((recommendation, index) => {
                   return (<RecommendationItem title={recommendation.title} desc={recommendation.desc} img={recommendation.img} key={index}></RecommendationItem>)
                 })}
               </TabPanel>
